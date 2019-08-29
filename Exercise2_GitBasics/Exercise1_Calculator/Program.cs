aa918e2c64e34bf3f46a22d3396c20128d326c2c
maxxum12@gmail.com <maxxum12@gmail.com>
2019-08-29 13:18:55 +0200

    Revert "Divide added"
    This reverts commit 38c68fec8086f8601aa1f70067e79b9a7d7ee366.

--- Exercise2_GitBasics/Exercise1_Calculator/Program.cs
+++ Exercise2_GitBasics/Exercise1_Calculator/Program.cs
@@ -35,10 +35,5 @@ namespace Exercise1_Calculator
         {
             return Math.Pow(x, exp); 
         }
-
-        public double Divide(double a, double b)
-        {
-            return a / b;
-        }
     }
 }
