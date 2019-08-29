ec251a5d947e7677392b56f3e9b836a24df4af6c
Elgaard1 <andreas.e.sorensen@hotmail.com>
2019-08-29 13:06:50 +0200

    Exercise 2 tilføjet

---
+++ Exercise2_GitBasics/Exercise1_Calculator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1_Calculator
+{
+    public class Calculator
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public double Add(double a, double b)
+        {
+            double sum = a + b;
+            return sum;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            double sum = a - b;
+            return sum;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            double sum = a * b;
+            return sum;
+        }
+
+        public double Power(double x, double exp)
+        {
+            return Math.Pow(x, exp); 
+        }
+    }
+}
---
+++ Exercise2_GitBasics/NUnitTestCalculator/NUnitTestCalculator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exercise1_Calculator;
+using NUnit.Framework;
+
+namespace NUnitTestCalculator
+{
+    public class CalculatorTest
+    {
+        private Calculator uut;
+        [SetUp]
+        public void setup()
+        {
+            //Arrange 
+            uut = new Calculator();
+        }
+        [Test]
+        public void Test_Add()
+        {
+            //Act 
+            double result = uut.Add(4, 4);
+
+            //Assert 
+            Assert.That(result, Is.EqualTo(8));
+        }
+
+        [Test]
+        public void Test_Subtract()
+        {
+            //Act 
+            double result = uut.Subtract(5, 4);
+
+            //Assert 
+            Assert.That(result, Is.EqualTo(1));
+        }
+        [Test]
+        public void Test_Multiply()
+        {
+            //Act 
+            double result = uut.Multiply(5, 4);
+
+            //Assert 
+            Assert.That(result, Is.EqualTo(20));
+        }
+
+        [Test]
+        public void Test_Power()
+        {
+            //Act 
+            double result = uut.Power(2, 2);
+
+            //Assert 
+            Assert.That(result, Is.EqualTo(4));
+        }
+    }
+}
